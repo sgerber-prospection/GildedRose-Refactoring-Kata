e0ac3a84ff2e71c0c1968c0733a32aa5a08771ff
Simon Gerber <simon.gerber@prospection.com.au>
2022-04-22 08:50:33 +1000

    Replace hard-coded strings with a category enum

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,19 +1,23 @@
 package com.gildedrose
 
+import com.gildedrose.GildedRose.ItemCategory.*
+
 class GildedRose(var items: Array<Item>) {
 
     enum class ItemCategory {
         BackstagePass,
         AgedCheese,
-        Legendary
+        Legendary,
+        Unremarkable
     }
 
     fun updateQuality() {
         for (i in items.indices) {
             val item = items[i]
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            val category = deriveCategory(item)
+            if (category != AgedCheese && category != BackstagePass) {
                 if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+                    if (category != Legendary) {
                         item.quality = item.quality - 1
                     }
                 }
@@ -21,7 +25,7 @@ class GildedRose(var items: Array<Item>) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1
 
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (category == BackstagePass) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1
@@ -37,15 +41,15 @@ class GildedRose(var items: Array<Item>) {
                 }
             }
 
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
+            if (category != Legendary) {
                 item.sellIn = item.sellIn - 1
             }
 
             if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (category != AgedCheese) {
+                    if (category != BackstagePass) {
                         if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                            if (category != Legendary) {
                                 item.quality = item.quality - 1
                             }
                         }
@@ -61,5 +65,16 @@ class GildedRose(var items: Array<Item>) {
         }
     }
 
+    private fun deriveCategory(item: Item): ItemCategory {
+        return if (item.name == "Aged Brie") {
+            AgedCheese
+        } else if (item.name.startsWith("Backstage passes")) {
+            return BackstagePass
+        } else if (item.name.startsWith("Sulfuras")) {
+            return Legendary
+        } else {
+            return Unremarkable
+        }
+    }
 }
 
